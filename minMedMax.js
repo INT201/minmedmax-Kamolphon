c56f1a5ffad011dbdd96b4230ef9bc3388b48094
Kamolphon <kamolphon.thon@kmutt.ac.th>
2022-09-11 16:26:15 +0700

    evekmp

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,22 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+  let eve
+  if (n1 > n2) {
+    eve = n2
+    n2 = n1 //n2จะโดนแทนที่ด้วยn1
+    n1 = eve
+  }
+  if (n1 > n3) {
+    eve = n3
+    n3 = n1
+    n1 = eve
+  }
+  if (n2 > n3) {
+    eve = n3
+    n3 = n2
+    n2 = eve
+  }
+  return { 'min': n1, 'mid': n2, 'max': n3 }
 }
 module.exports = minMedMax
